79eb37b883ba467441c03cd44f53d229316eb499
Duncanw93 <Duncanw1993@gmail.com>
2022-04-04 19:07:05 -0500

    Rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x53f9DfAe1A869CFFCDD73b6bb1C243a0D3bA92b4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
